9b746c99d52187975e018458e73267f067d47c37
Khatiwada <kushal.khatiwada@stu.jefferson.kyschools.us>
2018-09-18 10:35:49 -0400

    doing the name change

--- Project.py
+++ Project.py
@@ -1,6 +1,10 @@
-welcome = input("to play press Y to play, press X to quit")
-if welcome == "Y":
-    soccer_team = {
-        'team_name': 'Atlanta United FC' 
-else:
-    break
+soccer_team = {
+        'team_name': 'Atlanta United FC',
+        'team_color': 'red',
+        'team_players': ['Josef Martínez','Miguel Almirón','Ezequiel Barco','Héctor Villalba','Brad Guzan','Leandro González Pírez','Michael Parkhurst','Greg Garza','Eric Remedi','Darlington Nagbe','Franco Escobar']
+        }
+while True:
+        cmd = input("Do you wanna change the team name? ")
+        if cmd == 'y':
+            new_name = 'team_name'
+            
